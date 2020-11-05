c2e503f56c71d98a40d709dd41f139b52de6d5b1
Vilius <axelvilius.ovander@elev.ga.ntig.se>
2020-11-05 14:56:54 +0100

    initial commit

---
+++ railshooter/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Raylib_cs;
+
+namespace railshooter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int screenY = 800;
+            int screenX = 1200;
+            string scene = "intro";
+            Raylib.InitWindow(screenX, screenY, "poop B-)");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+
+                Raylib.ClearBackground(Color.BEIGE);
+
+
+
+                if (scene == "intro")
+                {
+                    Raylib.ClearBackground(Color.BEIGE);
+                    Raylib.DrawText("INTRO!!!!!", 100, 50, 20, Color.WHITE);
+                    Raylib.DrawText("Shoot B-)", (screenX / 2) - 100, screenY / 4, 50, Color.BLACK);
+
+
+                    if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
+                    {
+                        scene = "game";
+                    }
+
+                }
+                else if (scene == "game")
+                {
+
+                }
+
+
+                Raylib.EndDrawing();
+            }
+
+        }
+    }
+}
